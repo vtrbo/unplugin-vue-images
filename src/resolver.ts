4f6ad4056bc135698673a8dcbd1dcbea76d46208
Victor Bo <hi@vtrbo.cn>
2023-08-11 10:00:17 +0800

    chore: update resolver

--- src/resolver.ts
+++ src/resolver.ts
@@ -1,13 +1,13 @@
 import createDebugger from 'debug'
 import chokidar from 'chokidar'
-import { toLinesCase } from '@vtrbo/utils/string'
+import { ensureSuffix, toLinesCase } from '@vtrbo/utils/string'
 import { UNPLUGIN_NAME } from './core/constants'
 import { Content, type ImagesResolverOptions } from './core/content'
 
 const debug = createDebugger(`${UNPLUGIN_NAME}:resolver`)
 
 /**
- * Resolver for unplugin-vue-components and unplugin-auto-import
+ * Resolver for unplugin-vue-components
  *
  * @param options
  */
@@ -19,16 +19,17 @@ export function ImagesResolver(options: ImagesResolverOptions = {}) {
   content.setWatcher(chokidar.watch(content.options.dirs.map(dir => dir.path)))
 
   return (name: string) => {
-    debug('name =>', name)
-
     const lineName = toLinesCase(name)
+    debug('name =>', name, lineName)
+    debug('options =>', options)
 
-    if (lineName.startsWith(`${content.options.prefix}-`))
+    if (content.options.prefix && lineName.startsWith(ensureSuffix(content.options.prefix, '-')))
       return
 
     const image = content.searchImage(lineName)
     if (!image)
       return
+    debug('image =>', image)
 
     const resolverPath = `~images:${image.alias}/${image.name}?${image.ext}`
     debug('component path =>', resolverPath)
